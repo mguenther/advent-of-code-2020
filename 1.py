3f36ec6cbd3f39e3f9ad0d05d596c2d557fcde55
Markus Günther <markus.guenther@gmail.com>
2020-12-28 11:00:12 +0100

    Cleans up code for 1

--- 1.py
+++ 1.py
@@ -1,25 +1,28 @@
-# Schreibe hier Deinen Code :-)
+from typing import List
 
-numbers = [int(line) for line in open('/home/pi/Documents/AdventOfCode/1.in', 'r').readlines()]
 
-def solveForThree():
-    for x in range(len(numbers)):
-        for y in range(len(numbers)):
-            for z in range(len(numbers)):
-                if (x == y and y == z):
-                    continue
-                xn, yn, zn = numbers[x], numbers[y], numbers[z]
-                if xn + yn + zn == 2020:
-                    return xn * yn * zn
-
-def solveForTwo():
-    for l in range(len(numbers)):
-        for r in range(len(numbers)):
-            if l == r:
+def solve_for_two(numbers: List[int]) -> int:
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i == j:
                 continue
-            ln, rn = numbers[l], numbers[r]
-            if ln + rn == 2020:
-                return ln * rn
+            x, y = numbers[i], numbers[j]
+            if x + y == 2020:
+                return x * y
 
-print "Solution for 1st part: ", solveForTwo()
-print "Solution for 2nd part: ", solveForThree()
+
+def solve_for_three(numbers: List[int]) -> int:
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            for k in range(len(numbers)):
+                if i == j and j == k:
+                    continue
+                x, y, z = numbers[i], numbers[j], numbers[k]
+                if x + y + z == 2020:
+                    return x * y * z
+
+
+numbers = [int(line.strip()) for line in open('1.in', 'r').readlines()]
+
+print(solve_for_two(numbers))
+print(solve_for_three(numbers))
