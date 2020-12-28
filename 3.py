fe0b53f62b3ea6d2e4848f0f3594553178ff6c61
Markus Günther <markus.guenther@gmail.com>
2020-12-28 11:08:07 +0100

    Cleans up code for 3

--- 3.py
+++ 3.py
@@ -1,7 +1,7 @@
-grid = [l.strip() for l in open('/home/pi/Documents/AdventOfCode/3.in', 'r').readlines()]
+grid = [l.strip() for l in open('3.in', 'r').readlines()]
 
-maxX = len(grid[0])
-maxY = len(grid)
+max_x = len(grid[0])
+max_y = len(grid)
 
 slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
 ans = 1
@@ -10,12 +10,11 @@ for slope in slopes:
     position = (0,0)
     tree = 0
 
-    while position[1] < maxY:
-        print "Checking position: " + str(position)
-        if grid[position[1]][position[0] % maxX] == '#':
+    while position[1] < max_y:
+        if grid[position[1]][position[0] % max_x] == '#':
             tree += 1
         position = (position[0] + slope[0], position[1] + slope[1])
 
     ans *= tree
 
-print ans
+print(ans)
