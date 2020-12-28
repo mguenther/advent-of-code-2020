e50679daedfde0c70c2a57df45a9b09384c4ee64
Markus Günther <markus.guenther@gmail.com>
2020-12-28 11:13:44 +0100

    Cleans up code for 4

--- 4.py
+++ 4.py
@@ -1,19 +1,26 @@
-def skip_validation(value):
+from typing import Callable, Dict
+
+
+def skip_validation(value: str) -> bool:
     return True
 
-def is_valid_birth_year(value):
+
+def is_valid_birth_year(value: str) -> bool:
     birth_year = int(value)
     return birth_year >= 1920 and birth_year <= 2002
 
-def is_valid_issue_year(value):
+
+def is_valid_issue_year(value: str) -> bool:
     issue_year = int(value)
     return issue_year >= 2010 and issue_year <= 2020
 
-def is_valid_expiration_year(value):
+
+def is_valid_expiration_year(value: str) -> bool:
     expiration_year = int(value)
     return expiration_year >= 2020 and expiration_year <= 2030
 
-def is_valid_height(value):
+
+def is_valid_height(value: str) -> bool:
     is_valid = False
     if value.endswith('cm'):
         height = int(value.split('cm')[0])
@@ -23,7 +30,8 @@ def is_valid_height(value):
         is_valid = height >= 59 and height <= 76
     return is_valid
 
-def is_valid_hair_color(value):
+
+def is_valid_hair_color(value: str) -> bool:
     is_valid = True
     if value.startswith('#'):
         color_code = value.split('#')[1]
@@ -33,19 +41,22 @@ def is_valid_hair_color(value):
         is_valid = False
     return is_valid
 
-def is_valid_eye_color(value):
+
+def is_valid_eye_color(value: str) -> bool:
     return value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
-def is_valid_passport_id(value):
+
+def is_valid_passport_id(value: str) -> bool:
     is_valid = len(value) == 9
     for c in value:
         is_valid = is_valid and c in '0123456789'
     return is_valid
 
-def number_of_valid_passports(rules):
+
+def number_of_valid_passports(rules: Dict[str, Callable]) -> int:
     passport = {}
     nvalid = 0
-    for line in open('/home/pi/Documents/AdventOfCode/4.in', 'r'):
+    for line in open('4.in', 'r'):
         if not line.strip():
             # end-of-passport or end-of-file
             valid = True
@@ -61,6 +72,7 @@ def number_of_valid_passports(rules):
                 key, value = attr.split(':')
                 passport[key] = value
     return nvalid
+
 
 VALIDATION_FOR_FIRST = {
     'ecl': skip_validation,
@@ -82,5 +94,5 @@ VALIDATION_FOR_SECOND = {
     'hgt': is_valid_height
 }
 
-print "Solution to first part: " + str(number_of_valid_passports(VALIDATION_FOR_FIRST))
-print "Solution to second part: " + str(number_of_valid_passports(VALIDATION_FOR_SECOND))
+print(number_of_valid_passports(VALIDATION_FOR_FIRST))
+print(number_of_valid_passports(VALIDATION_FOR_SECOND))
