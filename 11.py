7959b09b3b4a023029097229c1e67d4a558dc5ec
Markus Günther <markus.guenther@gmail.com>
2020-12-28 11:50:42 +0100

    Cleans up code for 11

--- 11.py
+++ 11.py
@@ -2,20 +2,21 @@ import numpy as np
 
 
 def initial_assignment():
-    assignment = np.zeros(ROWS * COLS).reshape(ROWS, COLS)
-    for row in range(ROWS):
-        for col in range(COLS):
-            assignment[row][col] = T[LINES[row][col]]
+    assignment = np.zeros(rows * cols).reshape(rows, cols)
+    for row in range(rows):
+        for col in range(cols):
+            assignment[row][col] = T[lines[row][col]]
     return assignment
+
 
 def occupied_seats_part_two(row, col, assignment):
     occupied_seats = 0
-    for delta_row in DIRECTIONS:
-        for delta_col in DIRECTIONS:
+    for delta_row in directions:
+        for delta_col in directions:
             if delta_row == 0 and delta_col == 0:
                 continue
             probe_row, probe_col = row + delta_row, col + delta_col
-            while probe_row >= 0 and probe_row < ROWS and probe_col >= 0 and probe_col < COLS:
+            while probe_row >= 0 and probe_row < rows and probe_col >= 0 and probe_col < cols:
                 if assignment[probe_row][probe_col] == T['#']:
                     occupied_seats += 1
                     break
@@ -25,26 +26,28 @@ def occupied_seats_part_two(row, col, assignment):
                     probe_row, probe_col = probe_row + delta_row, probe_col + delta_col
     return occupied_seats
 
+
 def occupied_seats_adjacent_to(row, col, assignment):
     occupied_seats = 0
-    for delta_row in DIRECTIONS:
-        for delta_col in DIRECTIONS:
+    for delta_row in directions:
+        for delta_col in directions:
             if delta_row == 0 and delta_col == 0:
                 continue
             probe_row, probe_col = row + delta_row, col + delta_col
-            if probe_row < 0 or probe_row >= ROWS:
+            if probe_row < 0 or probe_row >= rows:
                 continue
-            if probe_col < 0 or probe_col >= COLS:
+            if probe_col < 0 or probe_col >= cols:
                 continue
             if assignment[probe_row][probe_col] == T['#']:
                 occupied_seats += 1
     return occupied_seats
 
+
 def next_assignment(assignment):
-    new_assignment = np.zeros(ROWS * COLS).reshape(ROWS, COLS)
+    new_assignment = np.zeros(rows * cols).reshape(rows, cols)
     number_of_changes = 0
-    for row in range(ROWS):
-        for col in range(COLS):
+    for row in range(rows):
+        for col in range(cols):
             occupied_seats = occupied_seats_part_two(row, col, assignment)
             if assignment[row][col] == T['L'] and occupied_seats == 0:
                 new_assignment[row][col] = T['#']
@@ -57,6 +60,7 @@ def next_assignment(assignment):
     assignment = new_assignment
     return (new_assignment, number_of_changes)
 
+
 def find_fixpoint_assignment(assignment):
     while True:
         assignment, number_of_changes = next_assignment(assignment)
@@ -64,13 +68,15 @@ def find_fixpoint_assignment(assignment):
             # we have reached the fixpoint
             return assignment
 
+
 def total_number_of_occupied_seats(assignment):
     t = 0
-    for row in range(ROWS):
-        for col in range(COLS):
+    for row in range(rows):
+        for col in range(cols):
             if assignment[row][col] == T['#']:
                 t += 1
     return t
+
 
 T = {
     'L': 1.0,
@@ -78,11 +84,11 @@ T = {
     '#': 3.0
 }
 
-DIRECTIONS = [-1, 0, +1]
+directions = [-1, 0, +1]
 
-LINES = [l.strip() for l in open('11.in', 'r').readlines()]
+lines = [l.strip() for l in open('11.in', 'r').readlines()]
 
-ROWS = len(LINES)
-COLS = len(LINES[0])
+rows = len(lines)
+cols = len(lines[0])
 
 print(total_number_of_occupied_seats(find_fixpoint_assignment(initial_assignment())))
