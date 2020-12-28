5d539fdeecc224284d2a6f2c6dde18f375372c82
Markus Günther <markus.guenther@gmail.com>
2020-12-28 12:08:32 +0100

    Cleans up code for 16

--- 16.py
+++ 16.py
@@ -1,29 +1,32 @@
-
 from functools import reduce
 from typing import Callable, Dict, List, Tuple
-
-import pprint
 
 
 def parse_constraints(parser_mode: int) -> bool:
     return bool((parser_mode >> 0) & 1)
 
+
 def parse_my_ticket(parser_mode: int) -> bool:
     return bool((parser_mode >> 1) & 1)
 
+
 def parse_nearby_tickets(parser_mode: int) -> bool:
     return bool((parser_mode >> 2) & 1)
+
 
 def to_range_function(range_: str) -> Callable:
     lower, upper = range_.split('-')
     loweri, upperi = int(lower), int(upper)
     return lambda x: x >= loweri and x <= upperi
 
+
 def to_constraints(ranges_: List[str]) -> List[Callable]:
     return [to_range_function(range_) for range_ in ranges_]
 
+
 def evaluate_constraint(probe: int, constraint: List[Callable]) -> bool:
     return True in map(lambda x: x(probe), constraint)
+
 
 def parse(filename: str) -> Tuple[Dict[str, List[Callable]], List[int], List[List[int]]]:
     parser_mode = 1
@@ -51,6 +54,7 @@ def parse(filename: str) -> Tuple[Dict[str, List[Callable]], List[int], List[Lis
             sys.exit(1)
     return constraints, my_ticket, nearby_tickets
 
+
 def find_candidates(valid_nearby_tickets: List[List[int]], constraints: Dict[str, List[Callable]]) -> Dict[int, List[str]]:
     possible_fields_for_index = {} # key: index, value: list of possible fields
     for idx in range(len(valid_nearby_tickets[0])):
@@ -64,12 +68,14 @@ def find_candidates(valid_nearby_tickets: List[List[int]], constraints: Dict[str
         possible_fields_for_index[idx] = possible
     return possible_fields_for_index
 
+
 def find_candidates_with_single_value_left(candidates: Dict[int, List[str]]) -> List[str]:
     idx_field_assignments = []
     for idx in candidates:
         if len(candidates[idx]) == 1:
             idx_field_assignments.append((idx, candidates[idx][0]))
     return idx_field_assignments
+
 
 def find_assignment(candidates: Dict[int, List[str]]) -> Dict[int, str]:
     assignment = {}
@@ -82,12 +88,14 @@ def find_assignment(candidates: Dict[int, List[str]]) -> Dict[int, str]:
                 candidates[idx] = [f for f in candidates[idx] if f != field]
     return assignment
 
+
 def indices_for_departure_fields(assignment: Dict[int, str]) -> List[int]:
     res = []
     for idx in assignment:
         if assignment[idx].startswith('departure'):
             res.append(idx)
     return res
+
 
 constraints, my_ticket, nearby_tickets = parse('16.in')
 valid_nearby_tickets = []
@@ -106,16 +114,10 @@ for nearby_ticket in nearby_tickets:
                 break
     if not invalid_ticket:
         valid_nearby_tickets.append(nearby_ticket)
-        
 
-print("Solution to part one: " + str(sum(invalid_number_per_ticket)))
+print(str(sum(invalid_number_per_ticket)))
 
 candidates = find_candidates(valid_nearby_tickets, constraints)
-
-pprint.pprint(candidates)
-
 assignment = find_assignment(candidates)
 
-pprint.pprint(assignment)
-
-print("Solution to part two: " + str(reduce(lambda x,y: x*y, map(lambda i: my_ticket[i], indices_for_departure_fields(assignment)))))
+print(str(reduce(lambda x,y: x*y, map(lambda i: my_ticket[i], indices_for_departure_fields(assignment)))))
