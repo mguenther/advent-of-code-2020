ee0bfe59c93b81d25de92bda8329d7e6c3cbdb70
Markus Günther <markus.guenther@gmail.com>
2020-12-28 11:25:17 +0100

    Cleans up code for 7

--- 7.py
+++ 7.py
@@ -1,12 +1,15 @@
-INPUT = '/home/pi/Documents/AdventOfCode/7.in'
+from typing import Any, Dict, List
 
-def edge(node, weight):
-    return { 'node': node, 'weight': weight }
 
-def adjacency_list(input_file):
+def edge(node: str, weight: int) -> Dict[str, Any]:
+    return { 
+        'node': node, 
+        'weight': weight 
+    }
 
+
+def adjacency_list(input_file: str) -> Dict[str, List[str]]:
     G = {}
-
     for line in open(input_file, 'r').readlines():
         lhs, rhs = line.split('contain')
         node = lhs.replace('bags', '').strip()
@@ -17,16 +20,13 @@ def adjacency_list(input_file):
                 weight = int(adjacent_node_with_weight.strip().split(' ')[0].strip())
                 adjacency_list.append(edge(adjacent_node, weight))
         G[node] = adjacency_list
-
     return G
 
-def predecessors_of(node, G):
 
+def predecessors_of(node: str, G: Dict[str, List[str]]) -> List[str]:
     Q = []
     S = []
-
     Q.append(node)
-
     while len(Q) != 0:
         node = Q.pop()
         for key in G.keys():
@@ -35,19 +35,17 @@ def predecessors_of(node, G):
                     Q.append(key)
                     if key not in S:
                         S.append(key)
-
     return S
 
-def weight_of(node, G):
 
+def weight_of(node: str, G: Dict[str, List[str]]) -> int:
     sum = 0
-
     for edge in G[node]:
         sum += edge['weight'] * (1 + weight_of(edge['node'], G))
-
     return sum
 
-G = adjacency_list(INPUT)
 
-print "Solution to first part: ", len(predecessors_of('shiny gold', G))
-print "Solution to second part: ", weight_of('shiny gold', G)
+G = adjacency_list('7.in')
+
+print(len(predecessors_of('shiny gold', G)))
+print(weight_of('shiny gold', G))
